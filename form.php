74fe307df3fdb3a0d966adc807dff4bb99cf7bc5
Natasha <natasha@elcultivo.mx>
2018-01-22 07:51:36 -0600

    ehercicio de validacion dwe formulario

---
+++ form.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>PRUEBA DE PHP</title>
+
+	</head>
+ 
+	<body>
+		
+		
+
+
+
+
+	
+		<?php
+		    	$nombre = $_GET['nombre'];
+
+				echo$nombre;
+
+				echo "<br>";
+
+				$email = $_GET['email'];
+
+				echo$email; 
+
+				
+
+
+		?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>PRUEBA DE PHP</title>
+
+	</head>
+ 
+	<body>
+
+		<form name="formulario" method="get" action="form.php">
+		       Nombre: <input type="text" name=" nombre" placeholder="name" required>
+
+		       Email: <input type="email" name=" email" placeholder="email" required>
+		       <input type="submit"/>
+		</form>
+	</body>
+</html>
+
+
+
+
